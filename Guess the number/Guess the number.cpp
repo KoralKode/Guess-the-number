7b0d4ae001606bc4c67ec3a02974a3a90838f492
Stepan Pushkarev <stepa.pushka@outlook.com>
2024-04-21 15:31:53 +0300

    Добавьте файлы проекта.

---
+++ Guess the number/Guess the number.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");//делаем возможным вывод русского текста
+    srand(time(NULL));//чтобы число всегда было рандомным
+    int number = rand()%10+1;//интервал от 1 до 10 включительно
+    int tries = 1;
+    
+    cout << "Введите сложность (easy, medium, hard): ";
+    string difficulty;
+    cin >> difficulty;
+    int max_tries;
+    if (difficulty == "easy") {
+        max_tries = 10;
+    }
+    else if (difficulty == "medium") {
+        max_tries = 5;
+    }
+    else {
+        max_tries = 3;
+    }
+    int guessed = 11;
+    cout << "Введите число: ";
+    cin >> guessed;
+    while (guessed != number) {
+        ++tries;
+        if (tries > max_tries) {
+            break;
+        }if (guessed == number + 1 || guessed == number - 1) {
+            cout << "Жарко" << endl;
+        }
+        else {
+            cout << "Холодно" << endl;
+        }
+        cout << "Введите ещё раз: ";
+        cin >> guessed;
+        
+    }
+    if (tries <= max_tries) {
+        cout << "Браво, вы справились за " << tries << " попытки";
+    }
+    else {
+        cout << "Увы,но ах...";
+    }
+}
+
